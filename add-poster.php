9a5166776e4ceff1e6172c7bac2ec8e6e5d7db27
Lukas Strnad <strnad.lukas@proton.me>
2023-03-18 17:09:29 +0100

    Add page

---
+++ add-poster.php
@@ -0,0 +1,5 @@
+<?php
+
+// TODO: Prepare the data and send it to db
+
+
--- add.php
+++ add.php
@@ -1,3 +1,32 @@
-<?php
+<!-- TODO: Css -->
 
-// TODO: Add poster page
+<!DOCTYPE html>
+<html lang="cs">
+	<head>
+		<meta charset="UTF-8">
+		<title>Poster</title>
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<body>
+		<form method="post" action="add-poster.php">
+			<label for="title">Nadpis:</label>
+			<input type="text" name="title" id="title">
+
+			<label for="description">Popis:</label>
+			<textarea name="description" id="description" maxlength="3000"></textarea>
+
+			<label for="category">Kategorie:</label>
+			<select name="category" id="category">
+				<option value="1">Kategorie 1</option>
+			</select>
+
+			<label for="price">Cena:</label>
+			<input type="text" name="price" id="price">
+
+			<label for="image">Obrázek:</label>
+			<input type="file" name="image" id="image" accept=".png,.jpg,.jpeg">
+
+			<button type="submit">Přidat</button>
+		</form>
+	</body>
+</html>
