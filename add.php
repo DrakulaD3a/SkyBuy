57d4f1b8dce5ae3471878796b06a854e3e882ed9
Lukas Strnad <strnad.lukas@proton.me>
2023-03-19 20:22:27 +0100

    Basic styling

--- add.php
+++ add.php
@@ -1,4 +1,10 @@
-<!-- TODO: Css -->
+<?php
+session_start();
+
+if (empty($_SESSION["user"])) {
+    header("Location: login.php");
+}
+?>
 
 <!DOCTYPE html>
 <html lang="cs">
@@ -8,25 +14,29 @@
 		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
 	<body>
-		<form method="post" action="add-poster.php">
-			<label for="title">Nadpis:</label>
-			<input type="text" name="title" id="title">
 
-			<label for="description">Popis:</label>
-			<textarea name="description" id="description" maxlength="3000"></textarea>
+		<main class="padding-1" >
+			<form method="post" action="add-poster.php" class="flex direction-column align-items justify-content gap-half" >
+				<label for="title">Nadpis:</label>
+				<input type="text" name="title" id="title">
 
-			<label for="category">Kategorie:</label>
-			<select name="category" id="category">
-				<option value="1">Kategorie 1</option>
-			</select>
+				<label for="description">Popis:</label>
+				<textarea name="description" id="description" maxlength="3000"></textarea>
 
-			<label for="price">Cena:</label>
-			<input type="text" name="price" id="price">
+				<label for="category">Kategorie:</label>
+				<select name="category" id="category">
+					<option value="1">Kategorie 1</option>
+				</select>
 
-			<label for="image">Obrázek:</label>
-			<input type="file" name="image" id="image" accept=".png,.jpg,.jpeg">
+				<label for="price">Cena:</label>
+				<input type="text" name="price" id="price">
 
-			<button type="submit">Přidat</button>
-		</form>
+				<label for="image">Obrázek:</label>
+				<input type="file" name="image" id="image" accept=".png,.jpg,.jpeg">
+
+				<button type="submit">Přidat</button>
+			</form>
+		</main>
+
 	</body>
 </html>
