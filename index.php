b6264bd0b5c96036e03c955312ffb5952613b04c
zvolaja <zvolaja@student.uzlabina.cz>
2023-03-21 17:56:43 +0100

    Just some small modifications to formatting

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php
 session_start();
 
-//SELECT * FROM posts INNER JOIN categories ON posts.category_id=categories.id WHERE categories.name = "" ORDER BY price;
+require_once "config.php";
 
 if (empty($_SESSION["username"])) {
     header("Location: login.php");
 }
+
+$db = new PDO(
+    "mysql:host=" . DB_HOST . ";dbname=" . DB_USERNAME,
+    DB_USERNAME,
+    DB_PASSWORD
+);
+
+//SELECT * FROM posts INNER JOIN categories ON posts.category_id=categories.id WHERE categories.name = "" ORDER BY price;
 
 if (isset($_POST["search"])) {
     if (isset($_POST["min"]) && isset($_POST["max"])) {
--- product.php
+++ product.php
@@ -2,3 +2,17 @@
 
 // FIXME: Check if id is valid
 $id = $_GET['id'];
+
+$db = new PDO(
+    "mysql:host=" . DB_HOST . ";dbname=" . DB_USERNAME,
+    DB_USERNAME,
+    DB_PASSWORD
+);
+
+$query = $db->prepare("SELECT * FROM `posts` WHERE `id` = ?");
+$query->execute([$id]);
+
+if (empty($query)) {
+    echo "Post not found";
+    exit;
+}
