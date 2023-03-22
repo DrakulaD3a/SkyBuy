f981f33e562a0bc25f84b5d3b407b564340a1fd4
DrakulaD3a <strnad.lukas@proton.me>
2023-03-22 17:19:13 +0100

    Making bigger max upload size

--- add.php
+++ add.php
@@ -1,8 +1,8 @@
 <?php
 
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+// Making bigger max upload size
+ini_set('post_max_size', '64M');
+ini_set('upload_max_filesize', '64M');
 
 session_start();
 
