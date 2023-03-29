94770a8c3b8bbda23c1009a2f1b9bed00beb4c85
DrakulaD3a <strnad.lukas@proton.me>
2023-03-29 16:31:49 +0200

    Starting the redesign

--- index.php
+++ index.php
@@ -4,87 +4,25 @@ session_start();
 require_once "config.php";
 
 if (empty($_SESSION["username"])) {
-    if (!empty($_COOKIE["username"])) {
-        $_SESSION["username"] = $_COOKIE["username"];
-    } else {
-        header("Location: login.php");
-    }
+  if (!empty($_COOKIE["username"])) {
+    $_SESSION["username"] = $_COOKIE["username"];
+  } else {
+    header("Location: login.php");
+  }
 }
 
 $db = new PDO(
-    "mysql:host=" . DB_HOST . ";dbname=" . DB_USERNAME,
-    DB_USERNAME,
-    DB_PASSWORD
+  "mysql:host=" . DB_HOST . ";dbname=" . DB_USERNAME,
+  DB_USERNAME,
+  DB_PASSWORD
 );
 
-?>
 
-<!DOCTYPE html>
-<!-- TODO: CSS, a lot -->
-<html lang="cs">
-  <head>
-    <meta charset="UTF-8"/>
-    <title>Bazoš</title>
-    <link rel="stylesheet" type="text/css" href="stylesheet.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Online bazar pro každého">
-  </head>
-  <body id="main-body">
-
-    <div id="header" class="flex space-between align-items-start padding-0-2" >
-      <p>Logo</p>
-
-      <div id="filter" class="flex space-between align-items bg-dark-blue gap-half visible" >
-        <form method="post" class="white">
-          <label for="search">Vyhledat:</label>
-          <input type="text" name="search" class="search" />
-          <label for="min">Cena od:</label>
-          <input type="number" name="min" id="min" />
-          <label for="max">do:</label>
-          <input type="number" name="max" id="max" />
-          <button type="submit">Vyhledat</button>
-        </form>
-        <div id="sort">
-          Řadit podle: 
-          <a class="no-text-decoration" href="index.php?sort=price">ceny</a> | 
-          <a class="no-text-decoration" href="index.php?sort=data">data</a>
-        </div>
-      </div>
-
-      <div id="profile" class="bg-dark-blue">
-        <span class="flex justify-content align-items padding-0-2 height-full" >
-          Profil
-        </span>
-        <div id="profile-content" >
-          <a href="logout.php">Odhlásit</a>
-          <a href="add.php">Přidat inzerát</a>
-          <a href="products.php">Vaše inzeráty</a>
-          <a href="index.php">Všechny inzeráty</a>
-        </div>
-      </div>
-
-    </div>
-
-    <div id="side-bar" class="visible flex align-items bg-dark-blue flex-column padding-1" >
-      <h3>Kategorie:</h3>
-      <a href='index.php' class='no-text-decoration category' >vše</a>
-<?php
-$query = $db->query('SELECT * FROM categories;');
-$categories = $query->fetchAll();
-
-foreach ($categories as $category) {
-  echo "<a href='index.php?category=" . $category["id"] ."' class='no-text-decoration category' >" . $category["name"] . "</a>";
-}
-?>
-    </div>
-
-    <main id="main" >
-<?php
 $qry = "SELECT * FROM posts ";
 $done = false;
 $arr = [];
 
-if (!empty($_POST["search"])){
+if (!empty($_POST["search"])) {
   $qry .= "WHERE title LIKE \"%" . $_POST["search"] . "%\" ";
   $done = true;
 }
@@ -96,9 +34,8 @@ if (!empty($_GET["category"])) {
 if (!empty($_POST["min"])) {
   $qry .= ($done ? "AND " : "WHERE ") . "price > " . $_POST["min"] . " ";
   $done = true;
-  
 }
-if(!empty($_POST["max"])){
+if (!empty($_POST["max"])) {
   $qry .= ($done ? "AND " : "WHERE ") . "price < " . $_POST["max"] . " ";
 }
 
@@ -107,45 +44,75 @@ $qry .= $_GET["sort"] == "price" ? "price ASC" : "date DESC";
 
 $query = $db->prepare($qry);
 
-if(!empty($arr)){
+if (!empty($arr)) {
   $query->execute($arr);
-}else{
+} else {
   $query->execute();
 }
 
-$objects = $query->fetchAll();
-
-
-$index = 0;
-
-foreach ($objects as $object) {
-  $class = "";
-  if ($index >= 3) {
-    $class = "border-top ";
-  }
-  if ($index % 3 != 2) {
-        $class = $class . "border-right";
-  }
-  $index++;
-
-  ?>
-  <a href="product.php?id=<?= $object["id"] ?>" class="flex align-items-start flex-column no-text-decoration padding-1 gap-half height-full <?= $class ?>">
-    <img src="data:image/png;base64,<?= $object["pic"] ?>" width="100%" class="image-post" />
-    <h3><?= $object["title"] ?></h3>
-    <br>
-    <h4><?= $object["price"]?> Kč</h4>
-    <p><?= strlen($object["description"]) > 500 ? substr($object["description"], 0, 500) . "..." : $object["description"]?></p>
-  </a>
-  <?php
-}
 ?>
-    </main>
 
-    <footer id="footer" class="flex align-items space-between bg-light-blue padding-1" >
-      <!-- TODO: footer -->
-      <p>footer</p>
-      <p>footer</p>
-    </footer>
+<!DOCTYPE html>
+<html lang="cs">
 
-  </body>
+<head>
+  <meta charset="UTF-8" />
+  <title>Sky-Buy</title>
+  <link rel="stylesheet" type="text/css" href="stylesheet.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="Online bazar pro každého">
+</head>
+
+<body id="main-body">
+
+  <div id="logo"></div>
+
+  <div>
+    <form id="search-bar">
+      <input type="text" maxlength="32" />
+      <button type="submit">Vyhledat</button>
+    </form>
+  </div>
+
+  <main>
+
+    <div id="categories">
+      <h3>Kategorie</h3>
+      <ul>
+        <li><a href="index.php">vše</a></li>
+        <?php
+        $catQuery = $db->query('SELECT * FROM categories;');
+        $categories = $catQuery->fetchAll();
+
+        foreach ($categories as $category) { ?>
+          <li><a href="index.php?category=<?= $category["id"] ?>" class=""><?= $category["name"] ?></a></li>
+        <?php
+        } ?>
+      </ul>
+    </div>
+
+    <div id="products">
+      <?php
+      $objects = $query->fetchAll();
+
+      foreach ($objects as $object) { ?>
+        <a href="product.php?id=<?= $object["id"] ?>">
+          <img src="data:image/png;base64,<?= $object["pic"] ?>" />
+          <h3><?= $object["title"] ?></h3>
+          <br>
+          <h4><?= $object["price"] ?> Kč</h4>
+          <p><?= strlen($object["description"]) > 500 ? substr($object["description"], 0, 500) . "..." : $object["description"] ?></p>
+        </a>
+      <?php
+      } ?>
+    </div>
+
+    <div id="filters">
+      <h3>Další filtry</h3>
+    </div>
+
+  </main>
+
+</body>
+
 </html>
