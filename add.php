0a52e0fe8eb7d30e4a3bc091969e8cf847cbdeff
zvolaja <zvolaja@student.uzlabina.cz>
2023-03-30 13:01:15 +0200

    whitespace fix && input padding

--- add.php
+++ add.php
@@ -31,7 +31,7 @@ if (isset($_POST["title"])) {
     $stmt = $db->prepare(
       "INSERT INTO `posts` (user_id, category_id, contact, pic, title, description, price, date) VALUES (?, ?, ?, ?, ?, ?, ?, ?);"
     );
-    $stmt->execute([$user_id, $category_id, $contact, base64_encode($image_raw), $title, $description, $price, date("Y-m-d H:i:s")]);
+    $stmt->execute([$user_id, $category_id, trim($contact), base64_encode($image_raw), trim($title), trim($description), $price, date("Y-m-d H:i:s")]);
 
     header("Location: index.php");
   }
