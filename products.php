dafaeb2224c0940c409af97ebc2252281469f656
Lukas Strnad <strnad.lukas@proton.me>
2023-03-31 21:50:15 +0200

    Half working

--- products.php
+++ products.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 
 require_once "config.php";
@@ -17,141 +18,159 @@ $db = new PDO(
   DB_PASSWORD
 );
 
+$query = $db->prepare("SELECT id FROM `users` WHERE username = ?");
+$query->execute([$_SESSION["username"]]);
+$user = $query->fetch();
+
+$query = $db->prepare('DELETE FROM posts WHERE id = ? and user_id = ?;');
+$query->execute([$_GET["product_id"], $user["id"]]);
+
+$qry = "SELECT * FROM posts WHERE user_id = ? ";
+$done = false;
+$arr = [$user["id"]];
+
+if (!empty($_POST["search"])) {
+  $qry .= "AND title LIKE \"%" . $_POST["search"] . "%\" ";
+}
+if (!empty($_GET["category"])) {
+  $qry .= "AND category_id = ? ";
+  array_push($arr, $_GET["category"]);
+}
+if (!empty($_POST["min"])) {
+  $qry .= "AND price > " . $_POST["min"] . " ";
+}
+if (!empty($_POST["max"])) {
+  $qry .= "AND price < " . $_POST["max"] . " ";
+}
+
+$qry .= "ORDER BY ";
+$qry .= $_GET["sort"] == "price" ? "price ASC" : "date DESC";
+
+$query = $db->prepare($qry);
+
+$query->execute($arr);
+
 ?>
 
 <!DOCTYPE html>
-<!-- TODO: CSS, a lot -->
 <html lang="cs">
 
 <head>
   <meta charset="UTF-8" />
   <title>Sky Buy</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="description" content="Online bazar pro každého">
   <link rel="stylesheet" type="text/css" href="stylesheet.css" />
   <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <script src="https://kit.fontawesome.com/d9f7f676cb.js" crossorigin="anonymous"></script>
+  <script defer src="js/scroll.js"></script>
+  <script defer src="js/img-size.js"></script>
 </head>
 
-<body id="main-body">
+<body id="index-body">
 
-  <div id="header" class="flex space-between align-items-start padding-0-2">
-    <p>Logo</p>
+  <div id="logo">
+    <img src="assets/logo.png" />
+  </div>
 
-    <div id="filter" class="flex space-between align-items bg-dark-blue gap-half visible">
-      <form method="post" class="white">
-        <label for="search">Vyhledat:</label>
-        <input type="text" name="search" class="search" />
-        <label for="min">Cena od:</label>
-        <input type="number" name="min" id="min" />
-        <label for="max">do:</label>
-        <input type="number" name="max" id="max" />
-        <button type="submit">Vyhledat</button>
-      </form>
-      <div id="sort">
-        Řadit podle:
-        <a class="no-text-decoration" href="products.php?sort=price">ceny</a> |
-        <a class="no-text-decoration" href="products.php?sort=data">data</a>
+  <div id="profile">
+    <div id="logo-butt">
+      <i class="fa-solid fa-user fa-2xl"></i>
+      <div id="logo-hover">
+        <div id="name"><?= $_SESSION["username"] ?></div>
+        <a href="logout.php">Odhlásit se</a>
+        <a href="add.php">Přidat inzerát</a>
       </div>
     </div>
 
-    <div id="profile" class="bg-dark-blue visible">
-      <span class="flex justify-content align-items padding-0-2 height-full">
-        Profil
-      </span>
-      <div id="profile-content">
-        <a href="logout.php">Odhlásit</a>
-        <a href="add.php">Přidat inzerát</a>
+    <div id="menu">
+      <i class="fa-solid fa-ellipsis fa-2xl"></i>
+      <div id="menu-hover">
         <a href="products.php">Vaše inzeráty</a>
         <a href="index.php">Všechny inzeráty</a>
       </div>
     </div>
   </div>
 
-  <div id="side-bar" class="visible flex align-items bg-dark-blue flex-column padding-1">
-    <h3>Kategorie:</h3>
-    <a href='products.php' class='no-text-decoration category'>vše</a>
-    <?php
-    $query = $db->query('SELECT * FROM categories;');
-    $categories = $query->fetchAll();
-
-    foreach ($categories as $category) {
-      echo "<a href='products.php?category=" . $category["id"] . "' class='no-text-decoration category' >" . $category["name"] . "</a>";
-    }
-    ?>
+  <div>
+    <form id="search-bar">
+      <input type="text" maxlength="50" name="search" placeholder="Audi TT" value="<?= $_GET["search"] ?>" />
+      <button type="submit"><i class="fa-solid fa-magnifying-glass fa-2xl"></i></button>
+    </form>
   </div>
 
   <main id="main">
-    <?php
 
-
-
-    $query = $db->prepare("SELECT id FROM `users` WHERE username = ?");
-    $query->execute([$_SESSION["username"]]);
-    $user = $query->fetch();
-
-    $query = $db->prepare('DELETE FROM posts WHERE id = ? and user_id = ?;');
-    $query->execute([$_GET["product_id"], $user["id"]]);
-
-
-    $qry = "SELECT * FROM posts WHERE user_id = ? ";
-    $done = false;
-    $arr = [$user["id"]];
-
-    if (!empty($_POST["search"])) {
-      $qry .= "AND title LIKE \"%" . $_POST["search"] . "%\" ";
-    }
-    if (!empty($_GET["category"])) {
-      $qry .= "AND category_id = ? ";
-      array_push($arr, $_GET["category"]);
-    }
-    if (!empty($_POST["min"])) {
-      $qry .= "AND price > " . $_POST["min"] . " ";
-    }
-    if (!empty($_POST["max"])) {
-      $qry .= "AND price < " . $_POST["max"] . " ";
-    }
-
-    $qry .= "ORDER BY ";
-    $qry .= $_GET["sort"] == "price" ? "price ASC" : "date DESC";
-
-
-    $query = $db->prepare($qry);
-
-    $query->execute($arr);
-
-    $objects = $query->fetchAll();
-
-
-    $index = 0;
-
-    foreach ($objects as $object) {
-      $class = "";
-      if ($index >= 3) {
-        $class = "border-top ";
-      }
-      if ($index % 3 != 2) {
-        $class = $class . "border-right";
-      }
-      $index++;
-
-    ?>
-      <div class="flex align-items-start flex-column no-text-decoration padding-1 gap-half height-full <?= $class ?>">
-        <img src="data:image/png;base64,<?= $object["pic"] ?>" width="100%" class="image-post" />
-        <h3><?= $object["title"] ?></h3>
-        <br>
-        <h4><?= $object["price"] ?> Kč</h4>
-        <p><?= strlen($object["description"]) > 500 ? substr($object["description"], 0, 500) . "..." : $object["description"] ?></p>
-        <a id="del-post" href="products.php?product_id=<?= $object["id"] ?>">smazat</a>
+    <div id="categories-wrapper">
+      <div id="search-bar2" class="hidden">
+        <form>
+          <button type="submit"><i class="fa-solid fa-magnifying-glass fa-xl"></i></button>
+          <input type="text" maxlength="32" name="search" />
+        </form>
       </div>
-    <?php
-    }
-    ?>
-  </main>
+      <div id="categories">
+        <h3>Kategorie</h3>
+        <ul>
+          <li><a href="index.php">vše</a></li>
+          <?php
+          $catQuery = $db->query('SELECT * FROM categories;');
+          $categories = $catQuery->fetchAll();
 
-  <footer id="footer" class="flex align-items space-between bg-light-blue padding-1">
-    <!-- TODO: footer -->
-    <p>footer</p>
-    <p>footer</p>
-  </footer>
+          foreach ($categories as $category) { ?>
+            <li><a href="index.php?category=<?= $category["id"] ?>" class=""><?= $category["name"] ?></a></li>
+          <?php
+          } ?>
+        </ul>
+      </div>
+    </div>
+
+    <div id="filters">
+      <div id="filters-logo-wrapper-wrapper" class="hidden">
+        <div id="filters-logo-wrapper">
+          <img src="assets/logo.png" id="filters-logo" />
+          <h2>Sky Buy</h2>
+        </div>
+      </div>
+      <div id="filters-inside">
+        <h3>Další filtry</h3>
+        <h5>Filtrovat:</h5>
+        <form method="post">
+          <div>
+            <label for="min">Cena od:</label>
+            <input type="number" name="min" id="min" />
+            <label for="max">do:</label>
+            <input type="number" name="max" id="max" />
+          </div>
+          <button type="submit">Vyhledat</button>
+        </form>
+        <h5>Řadit podle:</h5>
+        <ul>
+          <li><a href="index.php?sort=price">Cena</a></li>
+          <li><a href="index.php?sort=date">Datum</a></li>
+        </ul>
+      </div>
+    </div>
+
+    <div id="products">
+      <?php
+      $objects = $query->fetchAll();
+
+      foreach ($objects as $object) { ?>
+        <a href="product.php?id=<?= $object["id"] ?>&search=<?= $_GET["search"] ?>" class="post">
+          <img class="product-img" src="data:image/png;base64,<?= $object["pic"] ?>" />
+          <h3><?= $object["title"] ?></h3>
+          <h4><?= $object["price"] ?> Kč</h4>
+          <p><?= $object["description"] ?></p>
+          <a class="delete-btn" href="products.php?product_id=<?= $object["id"] ?>">
+            Smazat
+          </a>
+        </a>
+      <?php
+      } ?>
+
+    </div>
+
+  </main>
 
 </body>
 
